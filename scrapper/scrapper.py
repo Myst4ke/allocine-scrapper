24eed2a46121e3a2bd49bc231f7bb105ccf70297
Florian <102389838+Myst4ke@users.noreply.github.com>
2023-05-20 23:08:03 +0200

    Correcting errors

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -14,8 +14,8 @@ def url_to_parse(url=""):
 
 def parse_to_data(soup_list=[]):
     # Creation d"un dictionnaire 
-    films_dico = {"films_number" : 0, "films" : []}
     film_number = 0
+    films_dico = {"films_number" : film_number, "films" : []}
     for soup in soup_list:
         content = soup.find("main", {"id": "content-layout"}) # Contenu de la page
         films = content.findAll("li", {"class": "mdl"}) # Liste des films
@@ -33,26 +33,50 @@ def parse_to_data(soup_list=[]):
                 film_data["length"] = descritpion[1].strip()
                 film_data["type"] = descritpion[2].strip()
 
+
+
                 # Realisateur, Acteurs, Synopsis
-                film_data["director"] = film.find("div", {"class": "meta-body-item meta-body-direction"}).text.replace("\n"," ").strip()[3:]
-                actors = film.find("div", {"class": "meta-body-item meta-body-actor"}).text.replace("\n"," ").strip()[5:].split(",")
-                film_data["actors"] = [x.strip() for x in actors]
+
+                try: # Certains films n'ont pas de realisateur
+                    film_data["director"] = film.find("div", {"class": "meta-body-item meta-body-direction"}).text.replace("\n"," ").strip()[3:]
+                except:
+                    film_data["director"] = ""
+
+                try: # Certains films n'ont pas d'acteurs
+                    actors = film.find("div", {"class": "meta-body-item meta-body-actor"}).text.replace("\n"," ").strip()[5:].split(",")
+                    film_data["actors"] = [x.strip() for x in actors]
+                except:
+                    film_data["actors"] = []
+                
                 film_data["synopsis"] = film.find("div", {"class": "synopsis"}).text.replace("\n"," ").strip()
+
+
 
                 # Nombre de sceances
                 seance = film.find("a", {"class": "button button-sm button-inverse-full"}).text.replace("\n"," ").strip()
                 film_data["sessions"] = int(''.join(re.findall(r'\d', seance)))
 
+
+
                 # Notes : ['Presse 3,3', 'Spectateurs 3,9', 'Mes amis --']
                 ratings = film.findAll("div", {"class": "rating-item"})
+
+                # Nettoyage de la liste puis concaténation
+                ratings = [rate.text.replace("\n"," ").strip() for rate in ratings[:-1]]
+                ratings = " ".join(ratings)
+                
+                # Transformation des notes de str -> float si le champ Presse/Spectateur est présent 
+                # Ex:    Presse 3,3 Spectateurs 3,9 -> 3.3  3.9      |     Spectateurs 3,9 -> N/A  3.9
                 film_data["rating"] = {
-                    "critics": float(re.sub(r'(\d*),(\d*)',r'\1.\2', ratings[0].text.replace("\n"," ").strip()[-3:])),
-                    "audience": float(re.sub(r'(\d*),(\d*)',r'\1.\2', ratings[1].text.replace("\n"," ").strip()[-3:]))
+                    "critics": float(r) if (r := re.sub(r'Presse\s*(\d*),(\d*).*', r'\1.\2', ratings)) != ratings else 'N/A',
+                    "audience": float(r) if (r := re.sub(r'.*Spectateurs\s*(\d*),(\d*).*', r'\1.\2', ratings)) != ratings else 'N/A'
                 }
+                
                 film_number += 1
                 films_dico["films"].append(film_data)
-            except:
+            except Exception as e:
                 pass
+    
     films_dico["films_number"] = film_number
     return films_dico
     
@@ -76,6 +100,7 @@ def main():
                 soup = url_to_parse(url)
                 soup_list.append(soup)
             except:
+                print(f'failed to parse : {url}')
                 pass
         
         data = parse_to_data(soup_list)
